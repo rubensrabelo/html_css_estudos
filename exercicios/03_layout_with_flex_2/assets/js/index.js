8a4a88385e6b65adf1e4a5bd0b21a63a72e4efe4
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-05-21 08:45:07 -0300

    feat(ui): finish responsive flex layout

--- exercicios/03_layout_with_flex_2/assets/js/index.js
+++ exercicios/03_layout_with_flex_2/assets/js/index.js
@@ -1,7 +1,15 @@
 const toggleBtn = document.getElementById('toggleTheme');
 const body = document.body;
+const hamburgerBtn = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
 
 toggleBtn.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
   toggleBtn.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
+});
+
+hamburgerBtn.addEventListener('click', () => {
+  const isExpanded = hamburgerBtn.getAttribute('aria-expanded') === 'true';
+  hamburgerBtn.setAttribute('aria-expanded', !isExpanded);
+  navLinks.classList.toggle('show');
 });
